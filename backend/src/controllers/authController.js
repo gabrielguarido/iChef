dfdb64f839cb112f812689804db30f31a14f5a66
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-05-02 13:17:03 -0300

    Add auth/login API with JWT generation for validation

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/User');
 
 const router = express.Router();
 
+/**
+ * Generates JWT Auth Token
+ * @param {*} params 
+ */
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
+
+/**
+ * Register a new User
+ */
 router.post('/register', async (request, response) => {
   const { email } = request.body;
 
@@ -13,12 +30,39 @@ router.post('/register', async (request, response) => {
 
     const user = await User.create(request.body);
 
+    // Setting password as 'undefined' so it won't be returned to the user
     user.password = undefined;
 
-    return response.send({ user });
+    return response.send({
+      user,
+      token: generateToken({ id: user.id }),
+    });
   } catch {
     return response.status(400).send({ error: 'Registration failed' });
   }
 });
+
+/**
+ * Authenticate User's credentials - Login
+ */
+router.post('/authenticate', async (request, response) => {
+  const { email, password } = request.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user)
+    return response.status(400).send({ error: 'User not found' });
+
+  if (!await bcrypt.compare(password, user.password))
+    return response.status(400).send({ error: 'Invalid password' });
+
+  // Setting password as 'undefined' so it won't be returned to the user
+  user.password = undefined;
+
+  response.send({
+    user,
+    token: generateToken({ id: user.id }),
+  });
+})
 
 module.exports = app => app.use('/auth', router);
