fe597c52e30f7fdf517d58e4fb2d6f068303300f
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-05-02 12:10:06 -0300

    Add UserScheema

---
+++ backend/src/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
+
+const UserScheema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserScheema.pre('save', async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+const User = mongoose.model('User', UserScheema);
+
+module.exports = User;
